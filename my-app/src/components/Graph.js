d9ecf6d341d8d2e5da52d0a3b703a205bd69dd3a
Benjamin Capetillo <capetillobenjamin85@gmail.com>
2023-10-12 09:55:31 -0400

    Implemented scroll-activated zooming

--- my-app/src/components/Graph.js
+++ my-app/src/components/Graph.js
@@ -2,12 +2,21 @@ import { useEffect, state, useState, memo } from 'react'
 import Sidebar from '../components/Sidebar'
 import '../App.css'
 import GraphFunctions, { zoomIn } from '../math/graph.js'
+import { MapInteractionCSS } from 'react-map-interaction'
 
 export default function Graph({ functProp }) {
 
     const [ f, setF ] = useState(() => {});
     const [ xLength, setXLength ] = useState(4);
     const [ firstRun, setFirstRun ] = useState(true);
+    const [ value, setValue ] = useState({scale: 1, 
+                                          translation: { x: 0, y: 0 }
+                                          
+                                        
+                                          
+                                        
+                                        });
+
     const checkForF = () => {
         if(f) {
             console.log("Graph.js, f(3): " + f(3));
@@ -31,6 +40,9 @@ export default function Graph({ functProp }) {
     
     //Only on startup, not on rerender
     useEffect(() => {
+        window.addEventListener('resize', (e) => {
+            e.preventDefault();
+        });
         checkForF();
         setFirstRun(false);
     }, []);
@@ -49,9 +61,34 @@ export default function Graph({ functProp }) {
 
     return(
         <div id="graph-wrapper">
-            <button onClick={handleZoomIn} >+</button>
-            <button onClick={handleZoomOut} >-</button>
-            <canvas id="myCanvas" width={'863.39'} height={'606'} ></canvas>
+            <MapInteractionCSS
+                value={value}
+                translationBounds={ { xMin: 0, xMax: 0, yMin: 0, yMax: 0 } }
+                onChange={(newValue) => {
+                    console.log(JSON.stringify(newValue));
+                        if(newValue.scale < value.scale) {
+                            console.log("Zoomed Out");
+                            newValue.translation.x = value.translation.x;
+                            newValue.translation.y = value.translation.y;
+                            handleZoomOut();
+                        }
+                        else if(newValue.scale > value.scale) {
+                            console.log("Zoomed In");
+                            newValue.translation.x = value.translation.x;
+                            newValue.translation.y = value.translation.y;
+                            handleZoomIn();
+                        } else /*Pan occurred*/ {
+                        
+                        
+                        }
+                        newValue.scale = 1;
+                        setValue(newValue)
+                    }
+                }
+                                                        >
+        
+                <canvas id="myCanvas" /*width={'863.39'} height={'606'}*/ ></canvas>
+            </MapInteractionCSS>
             
             <Sidebar handleGraphClickProp={handleGraphClick} />
             
