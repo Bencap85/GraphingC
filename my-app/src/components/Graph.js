3778b5f9e33d81d486ddd2ec8ea9883a7a62ee03
Benjamin Capetillo <capetillobenjamin85@gmail.com>
2023-10-12 11:08:07 -0400

    Converted function based graph.js exports to class based

--- my-app/src/components/Graph.js
+++ my-app/src/components/Graph.js
@@ -1,11 +1,110 @@
 import { useEffect, state, useState, memo } from 'react'
 import Sidebar from '../components/Sidebar'
 import '../App.css'
-import GraphFunctions, { zoomIn } from '../math/graph.js'
+import { GraphClass } from '../math/graph.js'
 import { MapInteractionCSS } from 'react-map-interaction'
 
 export default function Graph({ functProp }) {
 
+    const [ f, setF ] = useState(() => {});
+    const [ xLength, setXLength ] = useState(4);
+    const [ graphObject, setGraphObject ] = useState(null);
+    const [ firstRun, setFirstRun ] = useState(true);
+    const [ value, setValue ] = useState({scale: 1, 
+                                          translation: { x: 0, y: 0 }
+                                          
+                                        
+                                          
+                                        
+                                        });
+
+
+    const checkForF = () => {
+        if(f) {
+            console.log("Graph.js, f(3): " + f(3));
+            let points = [];
+            for(let i = -1*(xLength/2); i <= (xLength/2); i += 0.25) {
+                points.push({ x: i, y: f(i) });
+            } 
+            graphObject.clearAndDrawBackground();
+            graphObject.plotPoints(points);
+        } else if(graphObject) {
+            graphObject.clearAndDrawBackground();
+        }
+    }
+    const handleGraphClick = (funct) => {
+        console.log("HandleGraphClick ran in App.js, funct(3) = : "+ funct(3));
+        setF((x) => (x) => {
+          return funct(x);
+        });
+    }
+    
+    
+    //Only on startup, not on rerender
+    useEffect(() => {
+        window.addEventListener('resize', (e) => {
+            e.preventDefault();
+        });
+        //
+        setGraphObject(new GraphClass(document.getElementById('myCanvas'), xLength));
+        //
+        checkForF();
+        setFirstRun(false);
+    }, []);
+    //On each render
+    useEffect(() => {
+        console.log("Re rendered");
+        if(graphObject)
+            graphObject.xLength = xLength;
+        checkForF();
+    });
+    
+    const handleZoomIn = () => {
+        setXLength(xLength - 2);
+    }
+    const handleZoomOut = () => {
+        setXLength(xLength + 2);
+    }
+
+    return(
+        <div id="graph-wrapper">
+            <MapInteractionCSS
+                value={value}
+                translationBounds={ { xMin: 0, xMax: 0, yMin: 0, yMax: 0 } }
+                onChange={(newValue) => {
+                    console.log(JSON.stringify(newValue));
+                        if(newValue.scale < value.scale) {
+                            console.log("Zoomed Out");
+                            newValue.translation.x = value.translation.x;
+                            newValue.translation.y = value.translation.y;
+                            handleZoomOut();
+                        }
+                        else if(newValue.scale > value.scale) {
+                            console.log("Zoomed In");
+                            newValue.translation.x = value.translation.x;
+                            newValue.translation.y = value.translation.y;
+                            handleZoomIn();
+                        } else /*Pan occurred*/ {
+                        
+                        
+                        }
+                        newValue.scale = 1;
+                        setValue(newValue)
+                    }
+                }
+                                                        >
+        
+                <canvas id="myCanvas" width={window.screen.width} height={window.screen.height}  ></canvas>
+            </MapInteractionCSS>
+            
+            <Sidebar handleGraphClickProp={handleGraphClick} />
+            
+        </div>
+        
+    )
+}
+
+/*
     const [ f, setF ] = useState(() => {});
     const [ xLength, setXLength ] = useState(4);
     const [ firstRun, setFirstRun ] = useState(true);
@@ -77,7 +176,7 @@ export default function Graph({ functProp }) {
                             newValue.translation.x = value.translation.x;
                             newValue.translation.y = value.translation.y;
                             handleZoomIn();
-                        } else /*Pan occurred*/ {
+                        } else {
                         
                         
                         }
@@ -87,7 +186,7 @@ export default function Graph({ functProp }) {
                 }
                                                         >
         
-                <canvas id="myCanvas" /*width={'863.39'} height={'606'}*/ ></canvas>
+                <canvas id="myCanvas" ></canvas>
             </MapInteractionCSS>
             
             <Sidebar handleGraphClickProp={handleGraphClick} />
@@ -97,3 +196,5 @@ export default function Graph({ functProp }) {
     )
     
 }
+
+*/
--- my-app/src/math/graph.js
+++ my-app/src/math/graph.js
@@ -1,3 +1,148 @@
+class GraphClass {
+    constructor(canvas, startingXLength) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.xLength = startingXLength;
+        this.width = canvas.width * window.devicePixelRatio;
+        this.height = canvas.height * window.devicePixelRatio;
+        //Set (actual canvas) dimensions
+        canvas.width = this.width;
+        canvas.height = this.height;
+    }
+
+    drawBackground() {
+        const scale = (this.width/this.xLength);
+        const yLength = this.findYLength(scale).yLength;
+        const yTopDiff = this.findYLength(scale).topDiff;
+    
+        const lineWidth = 0.3;
+        this.ctx.lineWidth = lineWidth;
+    
+        let xPoints = [];
+        for(let i = 0; i < this.xLength; i++) {
+            let xVal = (this.width/this.xLength)*i;
+            xPoints.push(xVal);
+        }
+        let yPoints = [];
+        for(let i = 0; i < yLength+2; i++) {
+            const yDiffVal = yTopDiff*scale;
+            const yVal = (scale*i)+yDiffVal;
+            yPoints.push(yVal);
+            
+        }
+        
+        for(let i = 0; i < xPoints.length; i++) {
+            this.ctx.beginPath();
+            if(i === this.xLength/2) {
+                this.ctx.strokeStyle = "black";
+                this.ctx.moveTo(xPoints[i], 0);
+                this.ctx.lineWidth = 3;
+                this.ctx.lineTo(xPoints[i], this.width);
+                this.ctx.closePath();
+                this.ctx.stroke();
+                continue;
+            }
+            this.ctx.lineWidth = lineWidth;
+            this.ctx.strokeStyle = 'black';
+            this.ctx.moveTo(xPoints[i], 0);
+            this.ctx.lineTo(xPoints[i], this.width);
+            this.ctx.closePath();
+            this.ctx.stroke();
+        }
+        for(let i = 0; i < yPoints.length; i++) {
+            this.ctx.beginPath();
+            if(i === yLength/2) {
+                this.ctx.moveTo(0, yPoints[i]);
+                this.ctx.lineTo(this.width, yPoints[i]);
+                this.ctx.lineWidth = 3;
+                this.ctx.closePath();
+                this.ctx.stroke();
+                continue;
+            }
+            this.ctx.lineWidth = lineWidth;
+            this.ctx.moveTo(0, yPoints[i]);
+            this.ctx.lineTo(this.width, yPoints[i]);
+            this.ctx.closePath();
+            this.ctx.stroke();
+            
+        }
+    }
+    plotPoints(points) {
+        const scale = (this.width/this.xLength);
+        const yLength = this.findYLength(scale);
+    
+        let pxPoints = [];
+        for(let i = 0; i < points.length; i++) {
+            let pointInPx = this.convertToPx(points[i], scale);
+            pxPoints.push(pointInPx);
+        }
+        console.log(points);
+        console.log(pxPoints);
+        
+       this.ctx.beginPath();
+       this.ctx.strokeStyle = "red";
+       this.ctx.lineWidth = 0.5;
+        for(let i = 0; i < pxPoints.length-1; i++) {
+            this.ctx.moveTo(pxPoints[i].x, pxPoints[i].y);
+            this.ctx.lineTo(pxPoints[i+1].x, pxPoints[i+1].y); 
+            this.ctx.stroke();
+        }
+        
+        this.ctx.stroke();
+        this.ctx.closePath();
+        
+    }
+    clearAndDrawBackground() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        this.drawBackground();
+    }
+    //Helpers
+    convertToPx(point, scale) {
+        let xMidInPx = this.width/2;
+        let yMidInPx = this.height/2;
+
+        let pxPoint = {x:0, y:0};
+
+        if(point.x === 0) {
+            pxPoint.x = xMidInPx;
+        } else if(point.x > 0) {
+            pxPoint.x = xMidInPx + (point.x*scale)
+        } else if(point.x < 0) {
+            pxPoint.x = xMidInPx - Math.abs(point.x*scale);
+        }
+
+        if(point.y === 0) {
+            pxPoint.y = yMidInPx;
+        } else if(point.y > 0) {
+            pxPoint.y = yMidInPx - (point.y*scale);
+        } else if(point.y < 0) {
+            pxPoint.y = yMidInPx + Math.abs(point.y*scale);
+        }
+        
+        return pxPoint;
+    }
+
+    findYLength(scale) {
+        let yLength = Math.floor(this.height/scale);
+        if(yLength % 2 !== 0) {
+            yLength--;
+        }
+
+        let diff = (this.height/scale)-yLength;
+        let topDiff = diff/2;
+        
+        return { yLength, topDiff };
+    }
+
+}
+
+module.exports = { GraphClass }
+
+
+
+
+/*
+
 const setUp = (graph, xLength, firstRun) => {
     let width, height;
     if(firstRun) {
@@ -153,3 +298,5 @@ const findYLength = (height, scale) => {
 }
 
 module.exports = { setUp, graph, clearGraph };
+
+*/
